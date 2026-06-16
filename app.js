23cec59a0912c8da2d73b48d1879c78f832fd1bf
Damian Wujec <wujecdamian12@gmail.com>
2026-06-16 20:16:37 +0200

    refactor: simplify database connection configuration in app.js

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-const path = require("node:path");
 const { Pool } = require("pg");
 const express = require("express");
 const session = require("express-session");
+const path = require("node:path");
 const passport = require("passport");
 const { error } = require("node:console");
 const LocalStrategy = require("passport-local").Strategy;
 require("dotenv").config();
 
+const connectionString = process.env.CONNECTION_STRING;
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
-  ssl: true,
+  connectionString,
 });
+
+//CONFIG
 
 const app = express();
 app.set("views", path.join(__dirname, "views"));
@@ -24,7 +22,23 @@ app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
+//Routes
+
 app.get("/", (req, res) => res.render("index"));
+///sign up
+app.get("/sign-up", (req, res) => res.render("sign-up-form"));
+app.post("/sign-up", async (req, res, next) => {
+  try {
+    await pool.query("INSERT INTO users (username, password) VALUES ($1,$2)", [
+      req.body.username,
+      req.body.password,
+    ]);
+    res.redirect("/");
+  } catch (err) {
+    return next(err);
+  }
+});
+//APP LISTEN
 
 app.listen(3000, (error) => {
   if (error) {
